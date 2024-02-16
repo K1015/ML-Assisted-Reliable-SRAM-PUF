cda1bff3308720b35bcf65559d799f178cb05b08
K1015 <57289207+K1015@users.noreply.github.com>
2024-02-16 15:57:11 -0600

    Update entropy_BCH_15_11_1_multiple_helper_data.py

--- Min-Entropy-Loss/entropy_BCH_15_11_1_multiple_helper_data.py
+++ Min-Entropy-Loss/entropy_BCH_15_11_1_multiple_helper_data.py
@@ -20,10 +20,11 @@ import json
 
 path = 'Data_Files/'
 
+#Codewords for BCH[15,11,1]
 with open(path + "Codewords_w.json") as json_file:
     Codewords_w = json.load(json_file)
     
-
+#Standard Array for BCH[15,11,1]
 with open(path + "std_array_p_complete.json") as json_file:
     std_array_p = json.load(json_file)
     
@@ -85,26 +86,25 @@ for predTemp in TempRange:
     std_array_p_BCH_unique = np.zeros([16,2048])
     keys = ['15','14','13','12','11','10','9','8','7','6','5','4','3','2','1','0']
 
-    for key in keys:					## Iterating over all x's 
+    for key in keys:					     ## Iterating over all x's 
         print("key -> ", key)
         for i in range(len(bins[key])):
             print(key, " -> ", ((bins[key][i] )))
-            for j in Codewords_w:				## Iterating over all w's
-                temp_p = int(bins[key][i], 2)^int(j,2)	## x xor w
+            for j in Codewords_w:			     ## Iterating over all w's
+                temp_p = int(bins[key][i], 2)^int(j,2)	     ## Temporary helper data p = x xor w
                 
                 temp_p_use = str( bin(temp_p)[2:].zfill(len(j))) 
                 if(no_of_ones<2**15):
-                    if (temp_p_use in chain.from_iterable(std_array_p)) == True:
+                    if (temp_p_use in chain.from_iterable(std_array_p)) == True:   ## Search for the temporary helper data in Standard Array
                         posi_in_p = ([(ix,iy) for ix, row in enumerate(std_array_p) for iy, k in enumerate(row) if k == temp_p_use])		# gives the position of p in std array
-                        std_array_p_BCH_unique[posi_in_p[0][0]][posi_in_p[0][1]]	= std_array_p_BCH_unique[posi_in_p[0][0]][posi_in_p[0][1]]+ 1 	# posi_in_p[0][0] - row index;; posi_in_p[0][1] - col. index
+                        std_array_p_BCH_unique[posi_in_p[0][0]][posi_in_p[0][1]] = std_array_p_BCH_unique[posi_in_p[0][0]][posi_in_p[0][1]]+ 1 	# posi_in_p[0][0] - row index;; posi_in_p[0][1] - col. index
                         if(std_array_p_BCH_unique[posi_in_p[0][0]][posi_in_p[0][1]]==1):
                             no_of_ones = no_of_ones +1				
                             count_p_each_bins[int(key)] = count_p_each_bins[int(key)] +1 
                 else:
                     break
-    		#print(" ----------------------------------------------------------- ")
-    	#print("bin",key, " -> ", count_p_each_bins[key])
-        sum_sp_qj = sum_sp_qj +  count_p_each_bins[int(key)] * q_j[15-int(key)]	# (Eqtn. 11)
+
+        sum_sp_qj = sum_sp_qj +  count_p_each_bins[int(key)] * q_j[15-int(key)]
     
     print(" sum_sp_qj - ", sum_sp_qj)
     
@@ -115,8 +115,7 @@ for predTemp in TempRange:
     minEntropy_loss[count] = minEntropy[count] - Cond_minEntropy[count]
     
     print("no_of_ones in std. array - ",no_of_ones)
-
-
+        
 #%%
 #minEntropy is the same as that in the fixed helper data case
 np.save(path + 'minEntropy_Loss_multiple_p.npy',minEntropy_loss)
